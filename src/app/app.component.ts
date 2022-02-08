3641f9d57e06381d5c6d1a3632d683e0cf0f454e
datthanh0202 <datthanh0202@gmail.com>
2022-02-08 17:15:46 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,27 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   //name = 'Angular ' + VERSION.major;
-  // DATA BINDING 
+  // DATA BINDING
   //InputType = 'text';
-  
+
   //EVENT BINDING
   //handler(event){
   //console.log('clicked',event);
-//};
+  //};
   //user = {
-    //name: 'Dat',
-    //age: 32
+  //name: 'Dat',
+  //age: 32
   //};
   name = 'Angular' + VERSION.major;
   show = false;
   user = {
-    name : 'John Snow', 
-    age : 25
-  }
-
+    name: 'John Snow',
+    age: 25,
+  };
+  isDanger = false;
+  isWarning = true;
 }
